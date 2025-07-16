d23fda37b650c1becdabb25d4fa903812bc4a3da
Tuvi11 <soujanyamaharudra02@gmail.com>
2025-07-16 21:44:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads')); // serve photos publicly
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 // Routes
 app.use('/api/posts', postRoutes);
