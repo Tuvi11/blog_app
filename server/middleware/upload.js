5f9f8d9fd819b772c256e67d0c1870aec15221d9
Tuvi11 <soujanyamaharudra02@gmail.com>
2025-07-25 13:34:12 +0530

    Update upload.js

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.originalname);
+    cb(null, uniqueName + path.extname(file.originalname));
   }
 });
 
