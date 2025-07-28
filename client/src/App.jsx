0d3955749163610e41cb5f2234f05b586f1b4bc3
Tuvi11 <soujanyamaharudra02@gmail.com>
2025-07-28 13:18:39 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,47 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import UserForm from './pages/UserForm';
 import BlogList from './pages/BlogList';
 import AdminLogin from './pages/AdminLogin';
 import AdminPanel from './pages/AdminPanel';
 import ProtectedRoute from './components/ProtectedRoute';
+import Footer from './components/Footer'; // import Footer
+import { useEffect, useState } from 'react';
 
-function App() {
+function AppWrapper() {
+  const location = useLocation();
+  const hideFooterOn = ['/admin', '/admin-login']; // no footer on admin pages
+
+  const shouldHideFooter = hideFooterOn.includes(location.pathname);
+
   return (
-    <Router>
+    <>
       <Routes>
         <Route path="/" element={<UserForm />} />
         <Route path="/blogs" element={<BlogList />} />
         <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin" element={
-          <ProtectedRoute>
-            <AdminPanel />
-          </ProtectedRoute>
-        } />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminPanel />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
+      {!shouldHideFooter && <Footer />} {/* Footer on all user pages */}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppWrapper />
     </Router>
   );
 }
 
 export default App;
+
+
+
